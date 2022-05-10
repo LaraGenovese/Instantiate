418b4f232af2b71dbec228af9d79e6b16e97e441
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-05-10 09:54:02 -0300

    Initial commit

---
+++ Assets/Script/Instantiator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    public void CloneObject()
+    {
+        int cantDeCubos = 0;
+
+        while(cantDeCubos < 3)
+        {
+            Instantiate(objectToClone);
+            cantDeCubos += 1;
+        }
+    }
+}
