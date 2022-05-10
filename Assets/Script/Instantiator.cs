e42ca60f33181e0c5d1f5c344b915b197fbfada5
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-05-10 10:44:28 -0300

    Class 10/5

--- Assets/Script/Instantiator.cs
+++ Assets/Script/Instantiator.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Instantiator : MonoBehaviour
 {
     public GameObject objectToClone;
+    public int CloneAmount;
+    public Text txtCloneAmount;
 
     public void CloneObject()
     {
+        CloneAmount = int.Parse(txtCloneAmount.text);
         int cantDeCubos = 0;
 
-        while(cantDeCubos < 3)
+        while(cantDeCubos < CloneAmount)
         {
             Instantiate(objectToClone);
             cantDeCubos += 1;
